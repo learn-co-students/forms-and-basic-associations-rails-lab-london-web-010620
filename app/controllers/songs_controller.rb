22549f60023c769ae8bea7d17c641ab36d4bdb15
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-08 14:32:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,7 +47,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:genre_id, :artist_name, :title, song_notes: [])
   end
 end
 
